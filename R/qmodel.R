8f31cd2367cde0918d601d27e7edc5d3b818cdb2
Bipin Deep Singh <bipindeepsingh@gmail.com>
2020-03-24 14:53:24 +0530

    updated package defs

--- R/qmodel.R
+++ R/qmodel.R
@@ -1,7 +1,7 @@
 #' @title Remove multicollinearity and run logistic regression with variable selection
 #'
 #' @description Returns list of variables removed from model due to multicolleniarity or variable selection, model object for logistic regression and comparison of key accuracy parameters between test and control set, 20% of data passed by default is used for testing
-#' @param sample_data data frame where 1st column is identifier(key) and 2nd column is target variable should be named flag, data should not have missing values and categorical variables should be one hot encoded
+#' @param data data frame where 1st column is identifier(key) and 2nd column is target variable should be named flag, data should not have missing values and categorical variables should be one hot encoded
 #' @param vif_lim maximum acceptable vif value for treating multicollinearity, default set to 10
 #' @param p_lim maximum acceptable p value for variable in logistic regression default 0.06
 #' @param t_sample ratio of data to be used for training
@@ -10,154 +10,148 @@
 
 
 
-qmodel<-function(sample_data,vif_lim=10,p_lim=0.06,t_sample=0.8) {
+qmodel<-function(data,vif_lim=10,p_lim=0.06,t_sample=0.8) {
 
-#########################SD Check########################
+  #########################SD Check########################
+  sd_frame<-data.frame()
+  j<-0
+  for(i in colnames(data[,-c(1,2)])){
+    j<-j+1
+    sd_frame[j,"var"]<-i
+    sd_frame[j,"sd"]<-stats::sd(data[,i],na.rm = TRUE)
+  }
+  sd_frame<-sd_frame[sd_frame$sd==0,]
 
-sd_frame<-data.frame()
-j<-0
-for(i in colnames(sample_data[,-c(1,2)])){
-  j<-j+1
-  sd_frame[j,"var"]<-i
-  sd_frame[j,"sd"]<-sd(sample_data[,i],na.rm = TRUE)
-}
-sd_frame<-as.data.table(sd_frame)
-sd_frame<-sd_frame[sd==0]
-sd_frame<-as.data.frame(sd_frame)
-
-rm_frame<-data.frame()
-j<-length(rownames(rm_frame))
-for(i in sd_frame[,1]){
-  j<-j+1
-  rm_frame[j,'var']<-i
-  rm_frame[j,'step']<-'sd_null'
-  sample_data[,i]<-NULL
-}
+  rm_frame<-data.frame()
+  j<-length(rownames(rm_frame))
+  for(i in sd_frame[,1]){
+    j<-j+1
+    rm_frame[j,'var']<-i
+    rm_frame[j,'step']<-'sd_null'
+    data[,i]<-NULL
+  }
 
 
-####################VIF Check########################
+  ####################VIF Check########################
 
-al<-alias(linm<-lm(formula = flag ~.,data =sample_data[,-c(1)]))
-al1<-as.data.frame(al$Complete)
+  al<-stats::alias(linm<-stats::lm(formula = flag ~.,data =data[,-c(1)]))
+  al1<-as.data.frame(al$Complete)
 
-j<-length(rownames(rm_frame))
-for(i in rownames(al1)){
-  j<-j+1
-  rm_frame[j,'var']<-i
-  rm_frame[j,'step']<-'alias'
-  sample_data[,i]<-NULL
-}
+  j<-length(rownames(rm_frame))
+  for(i in rownames(al1)){
+    j<-j+1
+    rm_frame[j,'var']<-i
+    rm_frame[j,'step']<-'alias'
+    data[,i]<-NULL
+  }
 
-linm<-lm(formula = flag ~.,data =sample_data[,-c(1)])
-vif<-data.frame(vif(linm))
-vif$col_name<-rownames(vif)
-vif<-as.data.table(vif)
-names(vif)<-c("vif","var")
-setcolorder(vif,c(2,1))
-vif<-vif[order(-vif)]
-vif<-as.data.frame(vif)
-
-j<-length(rownames(rm_frame))
-while (vif[1,2]>=vif_lim) {
-  j<-j+1
-  var<-vif[1,1]
-  rm_frame[j,'var']<-vif[1,1]
-  rm_frame[j,'step']<-paste0('VIF: ',vif[1,2])
-  sample_data[,var]<-NULL
-  linm<-lm(formula = flag ~.,data =sample_data[,-c(1)])
-  vif<-data.frame(vif(linm))
+  linm<-stats::lm(formula = flag ~.,data =data[,-c(1)])
+  vif<-data.frame(car::vif(linm))
   vif$col_name<-rownames(vif)
-  vif<-as.data.table(vif)
+  rownames(vif)<-c(1:length(rownames(vif)))
   names(vif)<-c("vif","var")
-  setcolorder(vif,c(2,1))
-  vif<-vif[order(-vif)]
-  vif<-as.data.frame(vif)
-}
+  vif<-vif[,c(2,1)]
+  vif<-vif[order(-vif$vif),]
+
+  j<-length(rownames(rm_frame))
+  while (vif[1,2]>=vif_lim) {
+    j<-j+1
+    var<-vif[1,1]
+    rm_frame[j,'var']<-vif[1,1]
+    rm_frame[j,'step']<-paste0('VIF: ',vif[1,2])
+    data[,var]<-NULL
+    linm<-stats::lm(formula = flag ~.,data =data[,-c(1)])
+    vif<-data.frame(car::vif(linm))
+    vif$col_name<-rownames(vif)
+    rownames(vif)<-c(1:length(rownames(vif)))
+    names(vif)<-c("vif","var")
+    vif<-vif[,c(2,1)]
+    vif<-vif[order(-vif$vif),]
+  }
 
 
-#########################Sampling###########################
+  #########################Sampling###########################
 
-set.seed(80085)
-index<-sample(1:nrow(sample_data),size = t_sample*nrow(sample_data))
-train<-sample_data[index,]
-test<-sample_data[-index,]
+  set.seed(80085)
+  index<-sample(1:nrow(data),size = t_sample*nrow(data))
+  train<-data[index,]
+  test<-data[-index,]
 
-##############################Logistic Model###########################
-base.glm<-glm(flag ~.,family=binomial(link='logit'),data=train[,-c(1)])
-base_sum<-summary(base.glm)
-coeff_base<-data.frame(base_sum$coefficients)
-coeff_base$var<-rownames(coeff_base)
-coeff_base<-as.data.table(coeff_base)
-setcolorder(coeff_base,c(5,1:4))
-names(coeff_base)<-c('var','estimate','std_err','z_value','p_val')
-coeff_base<-coeff_base[-c(1:1),]
-coeff_base<-coeff_base[order(-p_val),]
-coeff_base<-as.data.frame(coeff_base)
-roc_base<-roc(train$flag,base.glm$fitted.values,plot=FALSE)
-
-j<-length(rownames(rm_frame))
-while (coeff_base[1,5]>=p_lim) {
-  j<-j+1
-  var<-coeff_base[1,1]
-  train[,var]<-NULL
-  rm_frame[j,'var']<-coeff_base[1,1]
-  rm_frame[j,'step']<-paste0('p_val: ',coeff_base[1,5])
-  rm(coeff_base)
-  base.glm <- glm(flag ~.,family=binomial(link='logit'),data=train[,-c(1)])
+  ##############################Logistic Model###########################
+  base.glm<-stats::glm(flag ~.,family=stats::binomial(link='logit'),data=train[,-c(1)])
   base_sum<-summary(base.glm)
   coeff_base<-data.frame(base_sum$coefficients)
   coeff_base$var<-rownames(coeff_base)
-  coeff_base<-as.data.table(coeff_base)
-  setcolorder(coeff_base,c(5,1:4))
+  rownames(coeff_base)<-c(1:length(rownames(coeff_base)))
+  coeff_base<-coeff_base[,c(5,1:4)]
   names(coeff_base)<-c('var','estimate','std_err','z_value','p_val')
   coeff_base<-coeff_base[-c(1:1),]
-  coeff_base<-coeff_base[order(-p_val),]
-  coeff_base<-as.data.frame(coeff_base)
-  roc_base<-roc(train$flag,base.glm$fitted.values,plot=FALSE)
-}
+  coeff_base<-coeff_base[order(-coeff_base$p_val),]
+  roc_base<-pROC::roc(train$flag,base.glm$fitted.values,plot=FALSE)
 
-train$prob<-base.glm$fitted.values
-baseglm_roc<-data.table(recall=roc_base$sensitivities,specificity=roc_base$specificities,
-                        threshold=roc_base$thresholds,auc=roc_base$auc)
+  j<-length(rownames(rm_frame))
+  while (coeff_base[1,5]>=p_lim) {
+    j<-j+1
+    var<-coeff_base[1,1]
+    train[,var]<-NULL
+    rm_frame[j,'var']<-coeff_base[1,1]
+    rm_frame[j,'step']<-paste0('p_val: ',coeff_base[1,5])
+    rm(coeff_base)
+    base.glm <- stats::glm(flag ~.,family=stats::binomial(link='logit'),data=train[,-c(1)])
+    base_sum<-summary(base.glm)
+    coeff_base<-data.frame(base_sum$coefficients)
+    coeff_base$var<-rownames(coeff_base)
+    rownames(coeff_base)<-c(1:length(rownames(coeff_base)))
+    coeff_base<-coeff_base[,c(5,1:4)]
+    names(coeff_base)<-c('var','estimate','std_err','z_value','p_val')
+    coeff_base<-coeff_base[-c(1:1),]
+    coeff_base<-coeff_base[order(-coeff_base$p_val),]
+    roc_base<-pROC::roc(train$flag,base.glm$fitted.values,plot=FALSE)
+  }
 
-baseglm_roc$t_select<-baseglm_roc$recall+baseglm_roc$specificity
-baseglm_roc2<-baseglm_roc[t_select==max(t_select)]
-base_threshold<-baseglm_roc2$threshold
+  train$prob<-base.glm$fitted.values
+  baseglm_roc<-data.frame(recall=roc_base$sensitivities,specificity=roc_base$specificities,
+                          threshold=roc_base$thresholds)
 
-train$class<-ifelse(train$prob>=base_threshold,1,0 )
+  baseglm_roc$t_select<-baseglm_roc$recall+baseglm_roc$specificity
+  baseglm_roc2<-baseglm_roc[baseglm_roc$t_select==max(baseglm_roc$t_select),]
 
-acc_train<-sum(ifelse(train$class==train$flag,1,0))/length(train$trip_id)
+  base_threshold<-baseglm_roc2$threshold
 
-conf_mat_train<-data.frame(data='Train',
-                          tp=sum(ifelse(train$flag==1 & train$class==1,1,0)),
-                          fp=sum(ifelse(train$flag==0 & train$class==1,1,0)),
-                          tn=sum(ifelse(train$flag==0 & train$class==0,1,0)),
-                          fn=sum(ifelse(train$flag==1 & train$class==0,1,0)))
+  train$class<-ifelse(train$prob>=base_threshold,1,0 )
 
-precision_train<-conf_mat_train$tp/(conf_mat_train$tp+conf_mat_train$fp)
+  acc_train<-sum(ifelse(train$class==train$flag,1,0))/length(train$trip_id)
 
-recall_train<-conf_mat_train$tp/(conf_mat_train$tp+conf_mat_train$fn)
+  conf_mat_train<-data.frame(data='Train',
+                             tp=sum(ifelse(train$flag==1 & train$class==1,1,0)),
+                             fp=sum(ifelse(train$flag==0 & train$class==1,1,0)),
+                             tn=sum(ifelse(train$flag==0 & train$class==0,1,0)),
+                             fn=sum(ifelse(train$flag==1 & train$class==0,1,0)))
 
-specificity_train<-conf_mat_train$tn/(conf_mat_train$tn+conf_mat_train$fp)
+  precision_train<-conf_mat_train$tp/(conf_mat_train$tp+conf_mat_train$fp)
 
-test$prob<-predict(base.glm,test,type = 'response')
-test$class<-ifelse(test$prob>=base_threshold,1,0 )
-acc_test<-sum(ifelse(test$class==test$flag,1,0))/length(test$trip_id)
-conf_mat_test<-data.frame(data='Test',
-                          tp=sum(ifelse(test$flag==1 & test$class==1,1,0)),
-                          fp=sum(ifelse(test$flag==0 & test$class==1,1,0)),
-                          tn=sum(ifelse(test$flag==0 & test$class==0,1,0)),
-                          fn=sum(ifelse(test$flag==1 & test$class==0,1,0)))
-precision_test<-conf_mat_test$tp/(conf_mat_test$tp+conf_mat_test$fp)
-recall_test<-conf_mat_test$tp/(conf_mat_test$tp+conf_mat_test$fn)
-specificity_test<-conf_mat_test$tn/(conf_mat_test$tn+conf_mat_test$fp)
+  recall_train<-conf_mat_train$tp/(conf_mat_train$tp+conf_mat_train$fn)
 
-conf_mat<-rbind(conf_mat_train,conf_mat_test)
+  specificity_train<-conf_mat_train$tn/(conf_mat_train$tn+conf_mat_train$fp)
 
-comp_frame<-data.frame(matrix=c('Acc','precision','recall'),
-                       train=c(acc_train,precision_train,recall_train),
-                       test=c(acc_test,precision_test,recall_test))
+  test$prob<-stats::predict(base.glm,test,type = 'response')
+  test$class<-ifelse(test$prob>=base_threshold,1,0 )
+  acc_test<-sum(ifelse(test$class==test$flag,1,0))/length(test$trip_id)
+  conf_mat_test<-data.frame(data='Test',
+                            tp=sum(ifelse(test$flag==1 & test$class==1,1,0)),
+                            fp=sum(ifelse(test$flag==0 & test$class==1,1,0)),
+                            tn=sum(ifelse(test$flag==0 & test$class==0,1,0)),
+                            fn=sum(ifelse(test$flag==1 & test$class==0,1,0)))
+  precision_test<-conf_mat_test$tp/(conf_mat_test$tp+conf_mat_test$fp)
+  recall_test<-conf_mat_test$tp/(conf_mat_test$tp+conf_mat_test$fn)
+  specificity_test<-conf_mat_test$tn/(conf_mat_test$tn+conf_mat_test$fp)
 
-return(rm_frame,base.glm,conf_mat,comp_frame)
+  conf_mat<-rbind(conf_mat_train,conf_mat_test)
 
+  comp_frame<-data.frame(matrix=c('Acc','precision','recall'),
+                         train=c(acc_train,precision_train,recall_train),
+                         test=c(acc_test,precision_test,recall_test))
+
+  result<-list(rm_frame,base.glm,conf_mat,comp_frame)
+  return(result)
 }
--- R/var_treat.R
+++ R/var_treat.R
@@ -1,26 +1,25 @@
 #' @title Remove multicollinearity from data
 #'
 #' @description Returns list of variables removed due to multicolleniarity and treated data frame
-#' @param sample_data data frame where 1st column is identifier(key) and 2nd column is target variable should be named flag, data should not have missing values and categorical variables should be one hot encoded
+#' @param data data frame where 1st column is identifier(key) and 2nd column is target variable should be named flag, data should not have missing values and categorical variables should be one hot encoded
 #' @param vif_lim maximum acceptable vif value for treating multicollinearity, default set to 10
 #' @return data frame (rm_frame) with list of variables removed and data frame (data_treat) containing treated data
 #' @export
 
 
-var_treat<-function(sample_data,vif_lim=10) {
+var_treat<-function(data,vif_lim=10) {
+
 
   #########################SD Check########################
 
   sd_frame<-data.frame()
   j<-0
-  for(i in colnames(sample_data[,-c(1,2)])){
+  for(i in colnames(data[,-c(1,2)])){
     j<-j+1
     sd_frame[j,"var"]<-i
-    sd_frame[j,"sd"]<-sd(sample_data[,i],na.rm = TRUE)
+    sd_frame[j,"sd"]<-stats::sd(data[,i],na.rm = TRUE)
   }
-  sd_frame<-as.data.table(sd_frame)
-  sd_frame<-sd_frame[sd==0]
-  sd_frame<-as.data.frame(sd_frame)
+  sd_frame<-sd_frame[sd_frame$sd==0,]
 
   rm_frame<-data.frame()
   j<-length(rownames(rm_frame))
@@ -28,13 +27,13 @@ var_treat<-function(sample_data,vif_lim=10) {
     j<-j+1
     rm_frame[j,'var']<-i
     rm_frame[j,'step']<-'sd_null'
-    sample_data[,i]<-NULL
+    data[,i]<-NULL
   }
 
 
   ####################VIF Check########################
 
-  al<-alias(linm<-lm(formula = flag ~.,data =sample_data[,-c(1)]))
+  al<-stats::alias(linm<-stats::lm(formula = flag ~.,data =data[,-c(1)]))
   al1<-as.data.frame(al$Complete)
 
   j<-length(rownames(rm_frame))
@@ -42,17 +41,16 @@ var_treat<-function(sample_data,vif_lim=10) {
     j<-j+1
     rm_frame[j,'var']<-i
     rm_frame[j,'step']<-'alias'
-    sample_data[,i]<-NULL
+    data[,i]<-NULL
   }
 
-  linm<-lm(formula = flag ~.,data =sample_data[,-c(1)])
-  vif<-data.frame(vif(linm))
+  linm<-stats::lm(formula = flag ~.,data =data[,-c(1)])
+  vif<-data.frame(car::vif(linm))
   vif$col_name<-rownames(vif)
-  vif<-as.data.table(vif)
+  rownames(vif)<-c(1:length(rownames(vif)))
   names(vif)<-c("vif","var")
-  setcolorder(vif,c(2,1))
-  vif<-vif[order(-vif)]
-  vif<-as.data.frame(vif)
+  vif<-vif[,c(2,1)]
+  vif<-vif[order(-vif$vif),]
 
   j<-length(rownames(rm_frame))
   while (vif[1,2]>=vif_lim) {
@@ -60,17 +58,17 @@ var_treat<-function(sample_data,vif_lim=10) {
     var<-vif[1,1]
     rm_frame[j,'var']<-vif[1,1]
     rm_frame[j,'step']<-paste0('VIF: ',vif[1,2])
-    sample_data[,var]<-NULL
-    linm<-lm(formula = flag ~.,data =sample_data[,-c(1)])
-    vif<-data.frame(vif(linm))
+    data[,var]<-NULL
+    linm<-stats::lm(formula = flag ~.,data =data[,-c(1)])
+    vif<-data.frame(car::vif(linm))
     vif$col_name<-rownames(vif)
-    vif<-as.data.table(vif)
+    rownames(vif)<-c(1:length(rownames(vif)))
     names(vif)<-c("vif","var")
-    setcolorder(vif,c(2,1))
-    vif<-vif[order(-vif)]
-    vif<-as.data.frame(vif)
+    vif<-vif[,c(2,1)]
+    vif<-vif[order(-vif$vif),]
   }
 
-data_treat<-sample_data
-return(data_treat,rm_frame)
+
+result<-list(data,rm_frame)
+return(result)
 }
